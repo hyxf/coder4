0982fab38edb143e03425b77065403ee23125a71
seven <1162584980@qq.com>
2024-12-02 22:49:03 +0800

    init

--- src/extension.ts
+++ src/extension.ts
@@ -4,11 +4,32 @@ export function activate(context: vscode.ExtensionContext) {
 
 	console.log('Congratulations, your extension "coder4" is now active!');
 
-	const disposable = vscode.commands.registerCommand('coder4.helloWorld', () => {
+	const disposableFileLayout = vscode.commands.registerCommand('coder4.file.layout', () => {
 		vscode.window.showInformationMessage('Hello World from Coder4!');
 	});
 
-	context.subscriptions.push(disposable);
+	const disposableFileComponent = vscode.commands.registerCommand('coder4.file.component', () => {
+		vscode.window.showInformationMessage('Hello World from Coder4!');
+	});
+
+	const disposableFileLoading = vscode.commands.registerCommand('coder4.file.loading', () => {
+		vscode.window.showInformationMessage('Hello World from Coder4!');
+	});
+
+	const disposableFilePage = vscode.commands.registerCommand('coder4.file.page', () => {
+		vscode.window.showInformationMessage('Hello World from Coder4!');
+	});
+
+	const disposableTerminalProject = vscode.commands.registerCommand('coder4.terminal.project', () => {
+		vscode.window.showInformationMessage('Hello World from Coder4!');
+	});
+
+	context.subscriptions.push(
+		disposableFileLayout,
+		disposableFileComponent,
+		disposableFileLoading,
+		disposableFilePage,
+		disposableTerminalProject);
 }
 
-export function deactivate() {}
+export function deactivate() { }
