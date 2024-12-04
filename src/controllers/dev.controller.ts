bf4d69af200e74fa7fd2a4c856450c14e8631676
seven <1162584980@qq.com>
2024-12-05 02:03:50 +0800

    init

--- src/controllers/dev.controller.ts
+++ src/controllers/dev.controller.ts
@@ -1,39 +1,16 @@
 import { env, window } from "vscode";
 import { Config } from "../configs/config";
 
+/**
+ * dev controller
+ */
 export class DevController {
     constructor(private readonly config: Config) { }
 
-    async generateCodeString(): Promise<void> {
-        const editor = window.activeTextEditor;
-
-        if (!editor) {
-            window.showErrorMessage('No active text editor');
-            return;
-        }
-
-        const selection = editor.document.getText(editor.selection);
-
-        if (!selection.length) {
-            window.showErrorMessage('No selected text');
-            return;
-        }
-
-        const tabSize = this.config.tabSize;
-
-        const spaces = new RegExp(` {${tabSize}}`, 'g');
-
-        const snippetString = selection
-            .split(/\r?\n/)
-            .map(line => line.replace(/\$(?![\d{]|TM_)/g, '\\$').replace(spaces, '\t'));
-
-        env.clipboard.writeText(`${snippetString}`);
-
-        window.showInformationMessage(
-            'Code String has been copied into the clipboard',
-        );
-    }
-
+    /**
+     * generate snippet
+     * @returns 
+     */
     async generateSnippet(): Promise<void> {
         const editor = window.activeTextEditor;
 
