a1ebeaa91232668aaa04c7db16506bc8bd0e9cab
Seven <1162584980@qq.com>
2024-12-05 11:46:29 +0800

    init

--- src/extension.ts
+++ src/extension.ts
@@ -41,7 +41,7 @@ export function activate(context: vscode.ExtensionContext) {
 	const disposablePyProject = vscode.commands.registerCommand(`${EXTENSION_ID}.python.pyproject.toml`, () => fileController.newPyProject(context));
 	const disposableRequirements = vscode.commands.registerCommand(`${EXTENSION_ID}.python.requirements.txt`, () => fileController.newRequirements());
 	const disposableContextPyProject = vscode.commands.registerCommand(`${EXTENSION_ID}.python.context.pyproject.toml`, (args) => fileController.editPyProject(args));
-	const disposableContextRequirements = vscode.commands.registerCommand(`${EXTENSION_ID}.python.context.requirements.txt`, (args) => fileController.editPyProject(args));
+	const disposableContextRequirements = vscode.commands.registerCommand(`${EXTENSION_ID}.python.context.requirements.txt`, (args) => fileController.editRequirements(args));
 
 	//---
 	const devController = new DevController(config);
